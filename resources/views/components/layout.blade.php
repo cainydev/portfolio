acde31468b0c6a51ac653601b737f3051a094c9c
John Wagner <info@techbra.in>
2022-09-25 15:13:08 +0200

    Removed faulty vite import

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,8 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    @vite('resources/fonts/RobotoMono-Regular.ttf')
-
     {{-- Favicon --}}
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-big.png">
