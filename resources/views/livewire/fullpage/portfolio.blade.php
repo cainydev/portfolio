d7bc8a1ec51c2c285a7ddd71c4566fe7f20b69f3
cainytheslave <wajo432@gmail.com>
2024-03-20 15:30:01 +0100

    Fix grid and update dependencies

--- resources/views/livewire/fullpage/portfolio.blade.php
+++ resources/views/livewire/fullpage/portfolio.blade.php
@@ -14,19 +14,19 @@
                        subtitle="Klimakrise und Materialknappheit. Immer wieder bleiben auf Baustellen Baumaterialen übrig und können nicht weitervermittelt werden. Diese Seite haben wir für einen schwedischen Kunden gebaut, der dieses Problem lösen wollte."
                        year="2021"
                        :alternate="true">
-        <img class="object-cover lg:col-span-3 rounded-lg"
+        <img class="object-cover w-full h-full lg:col-span-3 rounded-lg"
              src="{{ Vite::asset('resources/images/portfolio/hemio_fullscreen.webp') }}"
              alt="Hemio Screenshot Fullscreen"
              width="380" height="214">
-        <img class="object-cover lg:col-span-2 rounded-lg"
+        <img class="object-cover w-full h-full lg:col-span-2 rounded-lg"
              src="{{ Vite::asset('resources/images/portfolio/hemio_login.webp') }}"
              alt="Hemio Screenshot Login"
              width="380" height="360">
-        <img class="object-cover lg:col-span-2 rounded-lg"
+        <img class="object-cover w-full h-full lg:col-span-2 rounded-lg"
              src="{{ Vite::asset('resources/images/portfolio/hemio_newadd.webp') }}"
              alt="Hemio Screenshot Neues Angebot erstellen"
              width="380" height="360">
-        <img class="object-cover lg:col-span-3 rounded-lg"
+        <img class="object-cover w-full h-full lg:col-span-3 rounded-lg"
              src="{{ Vite::asset('resources/images/portfolio/hemio_messenger.webp') }}"
              alt="Hemio Screenshot Nachrichten"
              width="380" height="214">
@@ -36,19 +36,19 @@
                        subtitle="Um die internen Prozesse in der Kräutermanufaktur Kräuter & Wege zu dokumentieren und zu optimieren, haben wir hier ein vollumfassendes Informationssystem entwickelt. Die Software umfasst das Management von Anlieferungen, Warenhaltung, Herstellungsprozesse und Analysen und ermöglicht ein nahtloses Zusammenarbeiten von Logistik und Produktion."
                        year="2022"
                        :alternate="false">
-        <img class="object-cover border-primary border-2 lg:col-span-3 rounded-lg"
+        <img class="object-cover w-full h-full border-primary border-2 lg:col-span-3 rounded-lg"
              src="{{ Vite::asset('resources/images/portfolio/kis_calc.webp') }}"
              alt="KIS Berechnungstool"
              width="380" height="180">
-        <img class="object-cover border-primary border-2 lg:col-span-2 rounded-lg"
+        <img class="object-cover w-full h-full border-primary border-2 lg:col-span-2 rounded-lg"
              src="{{ Vite::asset('resources/images/portfolio/kis_list.webp') }}"
              alt="KIS Listenansicht"
              width="380" height="180">
-        <img class="object-cover border-primary border-2 lg:col-span-2 rounded-lg"
+        <img class="object-cover w-full h-full border-primary border-2 lg:col-span-2 rounded-lg"
              src="{{ Vite::asset('resources/images/portfolio/kis_recipe.webp') }}"
              alt="KIS Rezepte"
              width="380" height="180">
-        <img class="object-cover border-primary border-2 lg:col-span-3 rounded-lg"
+        <img class="object-cover w-full h-full border-primary border-2 lg:col-span-3 rounded-lg"
              src="{{ Vite::asset('resources/images/portfolio/kis_stats.webp') }}"
              alt="KIS Statistiken"
              width="380" height="180">
